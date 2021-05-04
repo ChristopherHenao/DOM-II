403a177739ce8db0c4cab5b283947f4d36012154
ChristoherHenao <cq.henao@gmail.com>
2021-05-04 17:09:27 -0400

    mouseover event added to nav bar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+// Header Section
+const header = document.querySelector("header");
+const nav = header.querySelector("nav");
+const navTags = Array.from(nav.querySelectorAll("a"));
+
+nav.addEventListener("mouseover", function (event){
+    event.target.style.color = "red";
+
+setTimeout(function() {
+    event.target.style.color = "";
+      }, 500);
+}, false);
+
+
+
+
+console.log(header);
+console.log(navTags);
