54109113f35c0438019aea2fbb45e44c6fc7646a
ChristoherHenao <cq.henao@gmail.com>
2021-05-04 20:37:58 -0400

    wheel event added to document

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 // Your code goes here
 
+
+// Document Events
+        // Keydown Event
+document.addEventListener("keydown", event => {
+    if (event.isComposing || event.key === "Escape") {
+      return console.log("hello");
+    }
+});
+
+        // Wheel Event
+document.addEventListener('wheel', event => console.log(`${event} was activated`));
+
 // Header Section
+
+        // Mouseover Event
 const header = document.querySelector("header");
 const nav = header.querySelector("nav");
 const navTags = Array.from(nav.querySelectorAll("a"));
@@ -10,15 +24,14 @@ nav.addEventListener("mouseover", function (event){
 
 setTimeout(function() {
     event.target.style.color = "";
-      }, 500);
+      }, 1000);
 }, false);
 
-document.addEventListener("keydown", event => {
-    if (event.isComposing || event.key === "Escape") {
-      return console.log("hello");
-    }
-});
 
 
 console.log(header);
 console.log(navTags);
+
+// header.addEventListener("load", function (event){
+//     console.log("the page has loaded successfully")
+// })
